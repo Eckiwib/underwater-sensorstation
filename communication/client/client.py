41217ec75525f34a80eb3a4e5bb532caccc784c0
Eckiwib <eckhardwib@outlook.de>
2024-07-10 21:59:56 +0200

    made files usable

--- communication/client/client.py
+++ communication/client/client.py
@@ -1,16 +1,34 @@
 import socket
 import os
 import time
-
-#funktioniert das auch unter Linux?
-cwd = os.path.abspath(__file__)
-os.chdir(cwd)
+from random import randint
+import json
 
 buffer = 1024
 serverips = "Eckhards-PC", "EckhardsSchullaptop"
 port = 2106
+serverips_file = "serverips"
+sep = "=?sEp?="
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    sefile = open(serverips_file,"r")
+    serverips = sefile.read().split(sep)
+    sefile.close()
+    if serverips[0] == "":
+        serverips = ["Eckhards-PC","EckhardsSchullaptop"]
+        sefile = open(serverips_file,"a")
+        for ip in serverips:
+            sefile.write(f"{ip}{sep}")
+        sefile.close()
+except:
+    sefile = open(serverips_file,"w")
+    sefile.close()
+    sefile = open(serverips_file,"a")
+    for ip in serverips:
+        sefile.write(f"{ip}{sep}")
+    sefile.close()
 
 def sendstring(string):
         global s
@@ -44,3 +62,24 @@ def recvstring():
                     time.sleep(0.5)
         string = str(s.recv(buffer).decode())
         return string
+
+def getwattemp():
+    return randint(0, 100)
+
+def getairtemp():
+    return randint(0, 100)
+
+def getparts():
+    return randint(0, 100)
+
+while True:
+    wattemp = getwattemp()
+    lufttemp = getairtemp()
+    parts = getparts()
+    sendstring(wattemp)
+    sendstring(lufttemp)
+    sendstring(parts)
+    print(wattemp)
+    print(lufttemp)
+    print(parts)
+    time.sleep(10)
--- communication/server.py
+++ communication/server.py
@@ -1,6 +1,7 @@
 import socket
-import tkinter
+import tkinter as tk
 import os
+import json
 
 buffer = 1024
 port = 2106
@@ -12,11 +13,12 @@ def recvstring():
 def sendstring(string):
     client_socket.sendall(str(string).encode())
 
-cwd = os.path.abspath(__file__)
-os.chdir(cwd)
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), port))
 s.listen()
 client_socket, client_adress = s.accept()
 
+while True:
+    print("Wassertemperatur:", recvstring())
+    print("Lufttemperatur:", recvstring())
+    print("Partikel:", recvstring())    
